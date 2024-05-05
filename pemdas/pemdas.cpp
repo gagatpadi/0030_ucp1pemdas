7c283bfecd10de650d3f3f465f5c2a12800c34cf
gagatpadi <padiyogaradhana@gmail.com>
2024-05-05 11:52:42 +0700

    Add project files.

---
+++ pemdas/pemdas.cpp
@@ -0,0 +1,147 @@
+#include <iostream>
+using namespace std;
+
+
+
+
+// 1. Berikan contoh deklarasi variable. Berikan 3 variable dengan tipe data yang berbeda.
+// a. bool lulus;
+// b. int pengurangan;
+// c. float pembagian;
+
+
+// 2. Berikan contoh minimal satu impementasi dari conditional statement
+
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+   //  int bilangan;
+    // srand(time(0));
+
+    // bilangan = rand() % 10;
+    // cout << "Nilai awalnya: " << bilangan << endl;
+
+    // if (bilangan >= 5)
+    // {
+      //   bilangan = 2 * bilangan;
+    // }
+    // cout << "Nilai sekarang: " << bilangan;
+ // }
+
+// 3. Berikan contoh implementasi dari struct
+
+// #include <iostream>
+// using namespace std;
+
+// struct Mahasiswa {
+	// string nama;
+	// string umur;
+	// string alamat;
+// };
+
+// int main() {
+	// Mahasiswa mhs;
+	// cout << "Nama : ";
+	// cin >> mhs.nama;
+	// cout << "Umur : ";
+	// cin >> mhs.umur;
+	// cout << "Alamat : ";
+	// cin >> mhs.alamat;
+
+
+	// cout << endl;
+	// cout << "\n Nama : " << mhs.nama;
+	// cout << "\n Umur : " << mhs.umur;
+	// cout << "\n Alamat : " << mhs.alamat;
+
+// }
+
+// 4. Berikan contoh implementasi dari prosedur dan fungsi
+
+// #include <iostream>
+// using namespace std;
+
+// int bilA, bilB;
+
+// void input()
+// {
+	// cout << "Masukan Bilangan Pertama = ";
+	// cin >> bilA;
+	// cout << "Masukan bilangan kedua = ";
+	// cin >> bilB;
+// }
+
+// int penjumlahan(int a, int b)
+// {
+	// return a + b;
+
+// }
+
+// int pengurangan(int a, int b)
+// {
+	// return a - b;
+// }
+
+// int perkalian(int a, int b)
+// {
+	// return a * b;
+// }
+
+// float pembagian(int a, int b)
+// {
+	// return a / b;
+// }
+
+// int main()
+// {
+	// int pilihan;
+	// do
+	// {
+		// cout << "Kalkulator Sederhana" << endl;
+		// cout << "====================" << endl;
+		// cout << "1. Penjumlahan" << endl;
+		// cout << "2. Pengurangan" << endl;
+		// cout << "3. Perkalian  " << endl;
+		// cout << "4. Pembagian  " << endl;
+		// cout << "5. Keluar     " << endl;
+		// cout << "==============" << endl;
+		// cout << "Masukan Menu Pilihan : ";
+		// cin >> pilihan;
+
+		// switch (pilihan)
+		// {
+		// case 1:
+			// input();
+			// cout << "Hasil Penjumlahan = " << penjumlahan(bilA, bilB) << endl;
+			// break;
+		// case 2:
+			// input();
+			// cout << "Hasil Pengurangan = " << pengurangan(bilA, bilB) << endl;
+			// break;
+		// case 3:
+			// input();
+			// cout << "Hasil Perkalian = " << perkalian(bilA, bilB) << endl;
+			// break;
+		// case 4:
+			// input();
+			// cout << "Hasil Pembagian = " << pembagian(bilA, bilB) << endl;
+			// break;
+
+		// case 5:
+
+			// break;
+
+		// default:
+			// cout << "Pilihan anda salah" << endl;
+			// break;
+		// }
+		// system("pause");
+		// system("CLS");
+
+	// } while (pilihan |= 5);
+// }
+
+
+// 5. Berikan contoh implementasi dari Looping
